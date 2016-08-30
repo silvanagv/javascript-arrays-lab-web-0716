5515ac871fb4656e5a8f7aed8569864ed85a2ad5
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-08-30 14:58:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-const app = "I don't do much."
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function prependKitten(name) {
+  var newArray = []; // An new empty array
+for (var i = 0; i < kittens.length; i++) {
+    newArray[i] = kittens[i];
+}
+  newArray.unshift(name);
+  return newArray;
+}
+
+function appendKitten(name) {
+  var newArray = []; // An new empty array
+for (var i = 0; i < kittens.length; i++) {
+    newArray[i] = kittens[i];
+}
+  newArray.push(name);
+  return newArray;
+}
+
+
+function removeFirstKitten() {
+  var newArray = []; // An new empty array
+for (var i = 0; i < kittens.length; i++) {
+    newArray[i] = kittens[i];
+}
+  newArray.shift();
+  return newArray;
+}
+
+function removeLastKitten() {
+  var newArray = []; // An new empty array
+for (var i = 0; i < kittens.length; i++) {
+    newArray[i] = kittens[i];
+}
+  newArray.pop();
+  return newArray;
+}
